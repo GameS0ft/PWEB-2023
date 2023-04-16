fd49c17e81c9b03878b5af6b363249ac1dcd6548
Markus328 <markus328@tutanota.com>
2023-04-16 12:31:25 -0300

    Adicionado animações parallax

--- codigos/portfolio.js
+++ codigos/portfolio.js
@@ -3,3 +3,38 @@ function desmarcar_checkbox(){
     const checkbox = document.getElementById('toggle');
         checkbox.checked = false;
 }
+
+function scrollTrigger(selector, options = {}){
+  let els = document.querySelectorAll(selector)
+  els = Array.from(els)
+  els.forEach(el => {
+    addObserver(el,options)
+  })
+}
+function addObserver(el, options){
+    let observer = new IntersectionObserver((entries, observer) => { 
+      entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('active')
+      }
+      else {
+        entry.target.classList.remove('active')
+      }
+    })
+  }, options)
+  observer.observe(el)
+}
+
+function addTriggers(){
+scrollTrigger('.scroll-text', {
+    rootMargin: '-150px 300px'
+})
+scrollTrigger('.scroll-media', {
+    rootMargin: '-180px'
+})
+}
+
+window.addEventListener("load", () => {
+  addTriggers()
+})
+
