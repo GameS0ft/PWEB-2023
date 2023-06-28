08195ebcbf3646f7c8925d292f5119c8b9c1bbc9
abimaelProgammer <abmaelo21@gmail.com>
2023-06-28 17:11:34 -0300

    atualização da barra de progresso

--- Spotify2/Script/script.js
+++ Spotify2/Script/script.js
@@ -4,14 +4,29 @@ var loaded = false;
 var playBtn = document.getElementById('play-btn');
 var pauseBtn = document.getElementById('pause-btn');
 
+var teste = document.getElementById('teste');
+var progresso = document.getElementById('progress');
+
+
+function barraProgresso()
+{
+    var duracao = audioPlayer.duration;
+    var tempoAtual = audioPlayer.currentTime;
+    var tempoPorcento = duracao/100;
+    porcentoAtual = Math.floor(tempoAtual/tempoPorcento);
+    progresso.style.width = +porcentoAtual+`%`;
+
+
+}
+
+ 
 pauseBtn.addEventListener('click',(e)=>
 {
     e.preventDefault();
     playBtn.style.display = "inline";
     pauseBtn.style.display = "none";
     audioPlayer.pause();
-
-
+    barraProgresso();
 
     return false;
 });
@@ -22,10 +37,8 @@ playBtn.addEventListener('click',(e)=>
     playBtn.style.display = "none";
     pauseBtn.style.display = "inline";
     audioPlayer.play();
+    barraProgresso();
 
-
-
-    return false;
 });
 
 const playSong = (file) => {
@@ -36,13 +49,13 @@ const playSong = (file) => {
     }
     audioPlayer.play();
 
- 
+
     playBtn.style.display = "none";
     pauseBtn.style.display = "inline";
 }
 
 
-document.querySelectorAll('.main-col').forEach(item=> 
+document.querySelectorAll('.main-col').forEach(item=>
 {
     item.addEventListener('click', event =>{
         let image = item.getAttribute('data-image');
@@ -56,7 +69,7 @@ document.querySelectorAll('.main-col').forEach(item=>
             <img src= "`+image+`" />
             <h3>`+artist+`<br/><span>`+song+`</span></h3`;
 
-            playSong(File); 
+            playSong(File);
 
     });
 });
